c9718f60708d402d4389f57d19edd51b62079b7c
infominer <infominer@protonmail.com>
2019-07-04 11:32:21 -0400

    remove docs dir

--- docs/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "DIDecentral Contributors Guide: Minimal-Mistakes-Jekyll Gem-Based Deploy",
-        "excerpt":"I’m creating this resource while I do the initial setup for DIDecentral, the Organization, to have its own web-site, to accompany the website, decentralized-id.com, we are co-creating. This will make it easier for contributors to understand how this site works, or even for you to use this guide and create...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/didecentral-contributors-guide-minimal-mistakes/",
-        "teaser":null}]
